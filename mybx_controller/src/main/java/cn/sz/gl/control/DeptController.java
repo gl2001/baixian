55ad0e828a5fd166a4299301a8013545e1f38867
gl2001 <45632311@qq.com>
2017-12-07 15:09:23 +0800

    gl2001添加了一个Dept控制器

---
+++ mybx_controller/src/main/java/cn/sz/gl/control/DeptController.java
@@ -0,0 +1,10 @@
+package cn.sz.gl.control;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("dc")
+public class DeptController {
+
+}
