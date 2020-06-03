e59a9a8c9539c6a4a4bf8fba748035ae2f3e4ad0
gl2001 <45632311@qq.com>
2020-06-03 15:08:21 +0800

    deptcontroller加了一个方法

--- mybx_controller/src/main/java/cn/sz/gl/control/DeptController.java
+++ mybx_controller/src/main/java/cn/sz/gl/control/DeptController.java
@@ -7,4 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("dc")
 public class DeptController {
 
+	@RequestMapping("sh")
+	public String show() {
+		return "welcome";
+	}
 }
